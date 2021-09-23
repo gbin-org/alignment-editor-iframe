4d92482250aab459b12426f467d5674e1ac8564f
Mike Brinker <themikejr@gmail.com>
2021-09-23 12:07:26 -0400

    add url params for source, target, gloss, and links.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
 import "./App.css";
 import { AlignmentEditor, AlignmentProvider } from "alignment-editor-rcl";
 
+const getText = (type) => {
+  const url = new URL(window.location.href);
+  const textString = url.searchParams.get(`${type}Text`);
+  if (textString) {
+    return textString.split("|").map((textPiece) => {
+      return { text: textPiece };
+    });
+  }
+};
+
+const getSourceGlosses = () => {
+  const url = new URL(window.location.href);
+  const glosses = url.searchParams.get(`sourceGlosses`);
+  console.log(glosses);
+  if (glosses) {
+    return glosses
+      .split("|")
+      .map((glossPiece, position) => {
+        if (glossPiece !== "null") {
+          return { position: position, glossText: glossPiece };
+        }
+      })
+      .filter(Boolean);
+  }
+};
+
+const getLinks = () => {
+  const url = new URL(window.location.href);
+  const links = url.searchParams.get(`links`);
+  if (links) {
+    return links.split("|").map((linkText) => {
+      const parts = linkText.split(":");
+      const sourceLinks = parts[0].split(",").map((string) => Number(string));
+      const targetLinks = parts[1].split(",").map((string) => Number(string));
+      return { sources: sourceLinks, targets: targetLinks, type: "manual" };
+    });
+  }
+};
+
 function App() {
   return (
-    <div style={{ width: "500px",  overflow: "hidden" }}>
+    <div style={{ width: "500px", overflow: "hidden" }}>
       <AlignmentProvider>
         <AlignmentEditor
           defaultView="line"
-          sourceGlosses={[
-            { position: 2, glossText: "anyone" },
-            { position: 5, glossText: "hearer" },
-          ]}
-          sourceSegments={[
-            { text: "ὅτι" },
-            { text: "εἴ" },
-            { text: "τις" },
-            { text: "ἀκροατὴς" },
-            { text: "λόγου" },
-            { text: "ἐστὶν" },
-          ]}
-          targetSegments={[
-            { text: "For" },
-            { text: "if" },
-            { text: "anyone" },
-            { text: "is" },
-            { text: "a" },
-            { text: "hearer" },
-          ]}
-          userLinks={[
-            { sources: [0], targets: [1, 2], type: "manual" },
-            { sources: [3], targets: [5], type: "manual" },
-          ]}
+          sourceGlosses={getSourceGlosses()}
+          sourceSegments={getText("source")}
+          targetSegments={getText("target")}
+          userLinks={getLinks()}
           stateUpdatedHook={(a) => {
             console.log("STATE UPDATED", a);
           }}
