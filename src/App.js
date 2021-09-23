fde597329e3693b384653164e3565f3d95fd5a9e
Mike Brinker <themikejr@gmail.com>
2021-09-23 11:24:48 -0400

    First stab at working iframe.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { AlignmentEditor, AlignmentProvider } from "alignment-editor-rcl";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "500px",  overflow: "hidden" }}>
+      <AlignmentProvider>
+        <AlignmentEditor
+          defaultView="line"
+          sourceGlosses={[
+            { position: 2, glossText: "anyone" },
+            { position: 5, glossText: "hearer" },
+          ]}
+          sourceSegments={[
+            { text: "ὅτι" },
+            { text: "εἴ" },
+            { text: "τις" },
+            { text: "ἀκροατὴς" },
+            { text: "λόγου" },
+            { text: "ἐστὶν" },
+          ]}
+          targetSegments={[
+            { text: "For" },
+            { text: "if" },
+            { text: "anyone" },
+            { text: "is" },
+            { text: "a" },
+            { text: "hearer" },
+          ]}
+          userLinks={[
+            { sources: [0], targets: [1, 2], type: "manual" },
+            { sources: [3], targets: [5], type: "manual" },
+          ]}
+          stateUpdatedHook={(a) => {
+            console.log("STATE UPDATED", a);
+          }}
+        />
+      </AlignmentProvider>
     </div>
   );
 }
